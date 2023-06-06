b11665d624dea337ce139783568a1b1bce3d4eb9
Shobhitha-182000 <shobhithaas18@gmail.com>
2023-06-06 10:14:24 +0530

    js current changer

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello wold");
+ //GETeLEMENTbYID
+//  var text=document.getElementById("hh");
+// //  text.innerText="Simple";
+// console.log(text.innerText);
+// text.style.border='solid black';
+// var formId=document.getElementById("form");
+//formId.style.border="solid black";
+
+
+//GETELEMENTBYCLASSNAME
+
+ var list=document.getElementsByClassName("l1");
+console.log(list);
+console.log(list[1]);
+list[1].textContent='cart 2';
+list[1].style.fontWeight="solid bold";
+list[1].style.backgroundColor='yellow';
+// hrading.item
+// text.innerHTML=<h2>Details </h2>;
