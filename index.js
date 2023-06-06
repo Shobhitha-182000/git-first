0204fbf1ced4350eb8e16d7e0a52daac88940323
Shobhitha-182000 <shobhithaas18@gmail.com>
2023-06-06 10:34:31 +0530

    green border

--- index.js
+++ index.js
@@ -12,8 +12,21 @@
  var list=document.getElementsByClassName("l1");
 console.log(list);
 console.log(list[1]);
-list[1].textContent='cart 2';
-list[1].style.fontWeight="solid bold";
-list[1].style.backgroundColor='yellow';
+//list[1].textContent='cart 2';
+list[2].style.backgroundColor='green';
+//we cannot do like this using class we can use id or for loop
+// list.style.fontWeight="bold";
+// list.style.color='red';
+
+//  var listById=document.getElementById("items");
+//  listById.style.fontWeight='Solid';
+//  listById.style.color='black';
+
+ //or we can use for loop
+ for(let i=0;i<list.length;i++){
+    // list[i].style.backgroundColor="#f4f4f4";
+    list[i].style.fontWeight='bold';
+    list[i].style.color='black';
+ }
 // hrading.item
 // text.innerHTML=<h2>Details </h2>;
