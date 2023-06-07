137c462202394d3c4d2853568c12fe9d919efd4d
Shobhitha-182000 <shobhithaas18@gmail.com>
2023-06-07 10:59:33 +0530

    delete and edit

--- dom.js
+++ dom.js
@@ -1,6 +1,6 @@
 //TRAVERSING OF DOM================//
 
-var child=document.querySelector('#items');
+// var child=document.querySelector('#items');
 //parent node
 // console.log(child.parentNode);
 // child.parentNode.style.backgroundColor='#f4f4f4';
@@ -28,24 +28,84 @@ var child=document.querySelector('#items');
 
 //CREATE ELEMENT
 //creating div tag
-var div=document.createElement("div");
+// var div=document.createElement("div");
 //add class
-div.className="hello";
+// div.className="hello";
 //add id
-div.id="hello1";
+// div.id="hello1";
 //add attributes
-div.setAttribute('title','hello world');
+// div.setAttribute('title','hello world');
 //adding element;
-var element=document.createTextNode("first div");
-div.appendChild(element);
+// var element=document.createTextNode("first div");
+// div.appendChild(element);
 //inserting this element on heading
-var container=document.querySelector('header .container');
-var h1=document.querySelector('header h1');
-container.insertBefore(div,h1);
-console.log(div);
+// var container=document.querySelector('header .container');
+// var h1=document.querySelector('header h1');
+// container.insertBefore(div,h1);
+// console.log(div);
 
-// var title=document.querySelector('.title');
-// var card card-body=document.querySelector('div')
+ var form=document.getElementById("addForm");
+ var ul=document.getElementById("items");
+ var filter=document.getElementById("filter");
+//to perform action
+form.addEventListener("submit",create);
+//to perform delete action
+ul.addEventListener("click",removeItems);
+ //to search items 
+ filter.addEventListener("keyup",filterFunction);
+
+ //TO ADD ITEM
+ function create(c){
+    c.preventDefault();
+    
+    //to get element by input
+    var input=document.getElementById("item").value;
+
+    //to create new list to get input from after submit that will display into list
+        var li=document.createElement("li");
+        li.className="list-group-item";
+        li.appendChild(document.createTextNode(input));
+        ul.appendChild(li);
+    //to add delete button
+        var deleteButton=document.createElement("Button");
+        deleteButton.className="btn btn-danger btn-sm float-right delete";
+        deleteButton.appendChild(document.createTextNode("X"));
+        li.appendChild(deleteButton);
+        ul.appendChild(li);
+    //to add edit button 
+    var edit=document.createElement("button");
+    edit.className="btn btn-danger btn-sm float-right delete";
+    edit.appendChild(document.createTextNode("edit"));
+    li.appendChild(edit);
+    ul.appendChild(li);
+ }
+
+    //TO REMOVE ITEM
+function removeItems(c){
+    if(c.target.classList.contains("delete")){
+        if(confirm("Are you sure??")){//pop up msg
+            var deleteAction=c.target.parentElement;
+            ul.removeChild(deleteAction);
+        }    
+    }
+}
+
+//FILTER OR TO SEARCH
+function filterFunction(c){
+    //convert to lowerCase
+    var text=c.target.value.toLowerCase();
+    //get list of items
+    var itemLsit=ul.getElementsByTagName("li");
+    //converting htmlcollections to array
+    Array.from(itemLsit).forEach(function(i){
+        var itemName=i.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text)!=-1){
+            i.style.display='bold';
+        }else{
+            i.style.display='none';
+        }
+    })
+}
 
  
 
